d5b16b1742d522ef595f1b521072fd44b7b30b51
CHRISHLOH <john.johnson127721@gmail.com>
2023-04-25 17:22:48 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,11 +3,20 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("1", "1", (byte)15);
+        userService.saveUser("2", "2", (byte)25);
+        userService.saveUser("3", "3", (byte)35);
+        userService.saveUser("4", "4", (byte)45);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 }}
